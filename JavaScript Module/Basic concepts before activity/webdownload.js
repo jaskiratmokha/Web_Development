82c9dd9d6b02830cb782c3d3c22eb080d944ede6
Jaskirat Singh <jsmokha33@gmail.com>
2021-10-02 13:50:06 +0530

    first experience with downloading from web

---
+++ JavaScript Module/Basic concepts before activity/webdownload.js
@@ -0,0 +1,14 @@
+// axios node module to downlaod html from browser
+let minimist = require("minimist");
+let axios = require("axios");
+let fs= require("fs");
+
+// node webdownload.js --url=https://github.com/Jas-Script --dest=dest.html
+args=minimist(process.argv);
+let dwnldPromise = axios.get(args.url);
+dwnldPromise.then(function(response){
+    let html = response.data;
+    fs.writeFileSync(args.dest, html, "utf-8");
+}).catch(function(err){
+    console.log(err);
+})
